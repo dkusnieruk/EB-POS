df45949aa888dbfbf81737f1e830ef7f90f4c837
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-12-05 19:37:05 +0100

    update task

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let gallery2 = new SimpleLightbox('.gallery2 a',{captionsData:`alt`,captionDelay:250, swipeClose:true,});
+gallery.on('show.simplelightbox', function () {
+	
+});
