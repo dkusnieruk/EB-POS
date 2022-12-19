d4eceb4a83b74ad050fc93533da8e936ab162a63
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-12-19 06:55:08 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let gallery2 = new SimpleLightbox('.gallery2 a',{captionsData:`alt`,captionDelay:250, swipeClose:true,});
-gallery.on('show.simplelightbox', function () {
-	
+let gallery2 = new SimpleLightbox('.gallery2 a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+  swipeClose: true,
 });
+gallery.on('show.simplelightbox', function () {});
